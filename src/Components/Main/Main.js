d62710d51ceab9594c548dc29daf92084550e0c8
saebom <boma91@gmail.com>
2021-05-03 23:27:32 +0900

    Add: main page layout frame

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -6,6 +6,18 @@ const Main = () => {
     return (
         <div>
             <Nav />
+
+            <div className="main_container">
+                
+                <div className="main_left">
+                    <div className="user_lists">sdfsd</div>
+                    <div className="feeds">sdfds</div>
+                </div>
+
+                <div className="main_right">
+                    sdfsdfsdfsdfsfsdfds
+                </div>
+            </div>
         </div>
     );
 };
