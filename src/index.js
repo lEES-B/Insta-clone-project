fe4073b58d4b0cf5b6041a6a0f33e97bc797cc12
saebom <boma91@gmail.com>
2021-05-05 23:40:18 +0900

    Add: data.js 파일 추가

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
+import Data from './Data';
 
 let myInfo = {
   myId: 'myInstaId05',
@@ -13,11 +14,17 @@ let myInfo = {
   myImage: 'https://images.unsplash.com/photo-1615390993826-e9e3238aef51?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1246&q=80'
 }
 
+let feed = Data;
+
 function reducer(state = myInfo, action) {
   return state
 }
 
-const store = createStore(combineReducers({reducer}));
+function reducer2(state = feed, action) {
+  return state;
+}
+
+const store = createStore(combineReducers({reducer, reducer2}));
 
 ReactDOM.render(
   <React.StrictMode>
