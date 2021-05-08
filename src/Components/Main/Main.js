51ca6d8071d59d156b15c821ab83d891a7d687f0
saebom <boma91@gmail.com>
2021-05-09 05:50:56 +0900

    Add: smile css수정

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,30 +2,36 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Nav from '../Nav/Nav';
 import './Main.scss';
+import Feed from '../../FeedData';
 import { connect } from 'react-redux';
 
 const Main = (props) => {
-
+    
     let [feed, setFeed] = useState([
-        { 
-            poster_path: "/xGuOF1T3WmPsAcQEQJfnG7Ud9f8.jpg",       
-        }
+        { poster_path: "/xGuOF1T3WmPsAcQEQJfnG7Ud9f8.jpg" }
     ]);
 
     useEffect(() => {
-        axios.get('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1')
-            .then((result) => {
-                console.log(result.data.results) // arr 20개
 
-                setFeed(result.data.results)
-            })
-            .catch(() => { console.log('failure') })
-    }, [])
+        function gets() {
+            axios.get('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1')
+                .then((result) => {
+                    console.log(result.data) // arr 20개
+
+                    setFeed(result.data.results)
+                })
+                .catch(() => { console.log('failure') })
+        }
+        gets()
+        
+    }, []); 
+
 
     let [love, setLove] = useState(0);
     let [review, setReview] = useState('');
 
-    console.log(feed)
+    // console.log(feed)
+
     return (
         <div>
             <Nav />
@@ -61,7 +67,7 @@ const Main = (props) => {
                                             </div>
 
                                             <div className="img_info">
-                                                <img src= {"https://image.tmdb.org/t/p/w1280" + feed[i].poster_path} />
+                                                <img src= {"https://image.tmdb.org/t/p/w1280" + feed[0].poster_path} />
                                             </div>
 
                                             <div className="review_info">
